e6c113828bc8f0234bcce73e116753e9c606d5b7
arthurcornil <102752044+arthurcornil@users.noreply.github.com>
2025-01-29 20:53:40 +0100

    dev

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let { isOpen = $bindable() } = $props();
+
+    function closeModal() {
+        isOpen = false;
+    }
+</script>
+{#if isOpen}
+  <div
+    class="fixed inset-0 bg-black/80 flex items-center justify-center"
+    on:click={closeModal}
+    role="presentation"
+  >
+    <div
+      class="bg-white rounded-xl p-7 mx-5 w-2xl max-w-screen"
+      on:click|stopPropagation
+      role="dialog"
+      aria-modal="true"
+    >
+        <slot />
+    </div>
+  </div>
+{/if}
--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -1,20 +1,38 @@
 <script>
     import { IconArrowRight } from '@tabler/icons-svelte';
+    import { IconFlag } from '@tabler/icons-svelte';
+    import Modal from '$lib/components/Modal.svelte';
+
     const courses = [{
         id: 1,
         name: 'Introduction to programming',
-        description: 'Familiarize yourself with the basic concepts of programming.'
-    }, {
-        id: 2,
-        name: 'Algorithms & data structures',
-        description: 'Learn the basics of algorithms.'
+        description: 'Familiarize yourself with the basic concepts of programming.',
+        chapters: [{ id: 1,
+                name: "What's programming?"
+            }, { id: 2,
+                name: "Variables"
+            }, { id: 3,
+                name: "Conditions"
+            }]
+        }, {
+            id: 2,
+            name: 'Algorithms & data structures',
+            description: 'Learn the basics of algorithms.'
     }];
+    let selectedCourse;
+    let isCourseModalOpen = $state(false);
+
+    function openModal(courseId) {
+        isCourseModalOpen = true;
+        selectedCourse = courses.find(course => course.id === courseId);
+    }
 </script>
 
 <section class="grid max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-32 gap-y-6 md:gap-10">
-    <h1 class="text-3xl md:text-4xl lg:text-5xl text-white font-bold mb-4 md:mb-6">The basics</h1>
     {#each courses as course}
-        <a href="#" class="hover:scale-105 transition-transform duration-300">
+        <button class="hover:scale-103 transition-transform duration-300 cursor-pointer"
+            onclick={() => openModal(course.id)}
+        >
             <div class="w-full bg-gray h-auto md:h-32 flex flex-col md:flex-row p-5 md:px-7 rounded-xl items-center justify-between gap-4 md:gap-6">
                 <div class="flex flex-col md:flex-row items-center gap-4 md:gap-6 lg:gap-8 w-full">
                     <img src={`/course${course.id}.png`} alt="" class="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 flex-shrink-0">
@@ -31,6 +49,23 @@
                     <IconArrowRight stroke={4} class="text-white w-5 h-5 md:w-6 md:h-6"/>
                 </div>
             </div>
-        </a>
+        </button>
     {/each}
 </section>
+<Modal bind:isOpen={isCourseModalOpen}>
+    <h2 class="text-black text-xl md:text-2xl lg:text-3xl font-bold mb-5 text-black">
+        {selectedCourse.name}
+    </h2>
+    <div class="flex flex-col gap-3">
+        {#each selectedCourse.chapters as chapter}
+            <div class="flex items-center justify-between p-4 bg-gray-50 rounded-lg">
+                <span class="text-lg font-bold text-gray-800">
+                    {chapter.name}
+                </span>
+                <div class="bg-primary-400 rounded-full size-10 flex justify-center items-center">
+                    <IconArrowRight stroke={3} class="text-white w-5 h-5 md:w-6 md:h-6"/>
+                </div>
+            </div>
+        {/each}
+    </div>
+</Modal>
